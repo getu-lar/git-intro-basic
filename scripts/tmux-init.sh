2bb167b27a416f00c44fc0451ad81d6ce5043b1a
Lukas Angerer <lukas.angerer@getunik.com>
2013-11-10 07:25:31 -0500

    improved tmux init script

--- scripts/tmux-init.sh
+++ scripts/tmux-init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+SCRIPTPATH=$(dirname $(readlink -f $0))
+
 tmux new -d -s git-intro -n home
+tmux send-keys -t git-intro:home "xhost +local:adam" Enter
+tmux send-keys -t git-intro:home "xhost +local:bob" Enter
+tmux send-keys -t git-intro:home "xdg-open $SCRIPTPATH/../presentation.html" Enter
+tmux send-keys -t git-intro:home "clear" Enter
 tmux new-window -t git-intro -n adam
 tmux send-keys -t git-intro:adam "sudo su adam" Enter
 tmux new-window -t git-intro -n bob
@@ -8,8 +14,10 @@ tmux send-keys -t git-intro:bob "sudo su bob" Enter
 sleep 2s
 tmux send-keys -t git-intro:adam "reverse" Enter
 tmux send-keys -t git-intro:adam "eclipse &" Enter
+tmux send-keys -t git-intro:adam "cd" Enter
 tmux send-keys -t git-intro:adam "clear" Enter
 tmux send-keys -t git-intro:bob "reverse" Enter
 tmux send-keys -t git-intro:bob "eclipse &" Enter
+tmux send-keys -t git-intro:bob "cd" Enter
 tmux send-keys -t git-intro:bob "clear" Enter
 tmux attach -t git-intro
